bd4d8e7f65e6c8ef967331c370586b6fd87824ba
Shreyas <shreyassriram99@gmail.com>
2021-05-16 15:51:08 +0530

    Update regex

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -90,7 +90,7 @@ func ParseContributionsData(rawHTML string, year string) Contributions {
 
 // parseTotal function parses the total contributions
 func parseTotal(rawHTML string) int {
-	r, _ := regexp.Compile("[0-9,]+ contributions")
+	r, _ := regexp.Compile("[0-9,]+(\n| )*contributions")
 	totalContributionsRaw := r.FindString(rawHTML)
 	if totalContributionsRaw == "" {
 		return 0
